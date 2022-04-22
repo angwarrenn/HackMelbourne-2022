7a8c7c3f34aab1e3170aa5615b02ee1a6af6ec96
Stephanie <stephaniedhartono@gmail.com>
2022-04-22 23:41:11 +1000

    added schedule widget

--- front-end/src/Pages/Team.js
+++ front-end/src/Pages/Team.js
@@ -1,5 +1,19 @@
+import { Fragment } from 'react';
+
+// importing components
+import {
+  EventWidget,
+  ContactWidget,
+} from '../ScheduleComponents/ScheduleIndex'
+
 const Team = () => {
-    return <h1>Add Schedule</h1>;
+    return(
+      <Fragment>
+        <h1>Add Schedule</h1>
+        <EventWidget />
+        <ContactWidget />
+      </Fragment>
+    )
   };
   
   export default Team;
