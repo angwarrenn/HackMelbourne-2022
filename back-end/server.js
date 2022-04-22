bbdd2cb94d93b52eab28c42be2edb38e9f6507e9
Warren Ang <ang.warrenn@gmail.com>
2022-04-22 17:36:34 +1000

    post working

--- back-end/server.js
+++ back-end/server.js
@@ -3,10 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/test-get", (req, res) => {
+  res.json({ success: true });
 });
 
-app.get("/login", (req, res) => {
-  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+app.post("/test-post", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
 });
