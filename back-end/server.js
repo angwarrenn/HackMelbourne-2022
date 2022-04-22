7ef6ee2e9b711ab7230e067605d0aac53d26b1e5
Warren Ang <ang.warrenn@gmail.com>
2022-04-22 21:58:48 +1000

    password hashing

--- back-end/server.js
+++ back-end/server.js
@@ -6,21 +6,53 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/test-get", (req, res) => {
-  res.json({ success: true });
+// create connection
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
-app.post("/test-post", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+// connect to mysql
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySql Connected...");
 });
+
+const bcrypt = require("bcrypt");
 
 app.post("/login", (req, res) => {
   res.json(req.body);
 });
 
 app.post("/signup", (req, res) => {
-  res.json(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  bcrypt.hash(
+    password,
+    parseInt(process.env.BCRYPT_SALTROUNDS),
+    (err, hash) => {
+      if (err) {
+        res.json({ success: false, error: err });
+      }
+
+      const sql = `INSERT INTO user (email, password) VALUES ('${email}','${hash}')`;
+
+      connection.query(sql, (err) => {
+        if (err) {
+          res.json({ success: false, error: err });
+        }
+
+        res.json({ success: true });
+      });
+    }
+  );
 });
 
 app.listen(process.env.PORT, () => {
