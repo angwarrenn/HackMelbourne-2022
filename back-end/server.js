f550faf6bc0b7f5c7b62d3d0eb9dceb38eb98ce6
Warren Ang <ang.warrenn@gmail.com>
2022-04-22 17:10:47 +1000

    get api working

--- back-end/server.js
+++ back-end/server.js
@@ -6,3 +6,7 @@ const app = express();
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
+
+app.get("/login", (req, res) => {
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+});
