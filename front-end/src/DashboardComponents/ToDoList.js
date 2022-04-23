95cabadcbbba1b346d407d1e531208f250022345
Stephanie <stephaniedhartono@gmail.com>
2022-04-24 08:39:09 +1000

    edited todo list

--- front-end/src/DashboardComponents/ToDoList.js
+++ front-end/src/DashboardComponents/ToDoList.js
@@ -1,7 +1,13 @@
+import { AiOutlineSmile } from "react-icons/ai";
+
 export default function ToDoWidget(){
     return (
         <div className="todolist">
             <h4>To-Do:</h4>
+            <br></br><br></br><br></br><br></br>
+            <h5>Nothing To Do!</h5>
+            <br></br>
+            <h6><AiOutlineSmile/></h6>
         </div>
     );
 }
