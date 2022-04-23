2716552655546f98a41a2ad8f7e8b24e7c08e150
Warren Ang <ang.warrenn@gmail.com>
2022-04-23 18:13:46 +1000

    login fix

--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,43 +1,55 @@
-import React, { useState } from 'react';
-import Axios from 'axios';
+import React, { useState } from "react";
+import Axios from "axios";
 
-import loghero from './loginhero.png';
+import loghero from "./loginhero.png";
 
-function LogIn(){
-  const url = "https://a7d5-125-63-30-143.au.ngrok.io/login"
-  const [data,setData] = useState({
-    email:"",
+function LogIn() {
+  const url = "https://a7d5-125-63-30-143.au.ngrok.io/login";
+  const [data, setData] = useState({
+    email: "",
     password: "",
-  })
+  });
 
-  function submit(e){
+  function submit(e) {
     e.preventDefault();
-    Axios.post(url,{
-      email:data.email,
+    Axios.post(url, {
+      email: data.email,
       password: data.password,
-    })
-      .then(res=>{
-        console.log(res.data)
-      })
+    }).then((res) => {
+      console.log(res.data);
+    });
   }
 
-  function handle(e){
-    const newdata = {...data}
-    newdata[e.target.id] = e.target.value
-    setData(newdata)
-    console.log(newdata)
+  function handle(e) {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+    console.log(newdata);
   }
-  return(
+
+  return (
     <div>
-      <div className='login'>
+      <div className="login">
         <h1>LOGIN</h1>
-        <form onSubmit={(e)=> submit(e)}>
-          <input onChange={(e)=>handle(e)} id ="email" value = {data.email} placeholder='email' type="text"></input>
-          <input onChange={(e)=>handle(e)} id ="password" value = {data.password} placeholder='password' type="password"></input>
-          <button className='button'>Submit</button>
+        <form onSubmit={(e) => submit(e)}>
+          <input
+            onChange={(e) => handle(e)}
+            id="email"
+            value={data.email}
+            placeholder="email"
+            type="text"
+          ></input>
+          <input
+            onChange={(e) => handle(e)}
+            id="password"
+            value={data.password}
+            placeholder="password"
+            type="password"
+          ></input>
+          <button className="button">Submit</button>
         </form>
       </div>
-      <img src={ loghero } alt="Login Hero" className='herolog'></img>
+      <img src={loghero} alt="Login Hero" className="herolog"></img>
     </div>
   );
 }
